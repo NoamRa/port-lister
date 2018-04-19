be5916e15cfaaf4ff6c6921a43775bf503885687
NoamR <noamraby@gmail.com>
2018-04-19 22:47:45 +0300

    transform raw output to json with portm process name and pid

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
-var exec = require('child_process').exec;
+const exec = require('child_process').exec;
 const express = require('express');
 const app = express();
 
 const conf = require('./conf.js');
 
+const template = 
 
 app.get("/", (req, res) => {
     let promise = new Promise((res, rej) => {
@@ -11,12 +12,40 @@ app.get("/", (req, res) => {
             if (err) {
                 rej(err)
             }
-            // console.log(stdout)
             res(stdout);
         });
     })
+    // transform command output to json with port, process name and process id
+    .then(rawResults => {
+        const resArr = rawResults.split("\n");
+        let serverDataList = resArr.map(line => {
+            return line.split(' ').filter(x => 
+                !!x && 
+                x !== '0' && 
+                x !== ':::*' && x !== '0.0.0.0:*' 
+                && x !== 'LISTEN' && 
+                x !== 'tcp' && x !== 'tcp6')
+        });
+        const portList = serverDataList.map(line => line[0] ? line[0].slice(line[0].lastIndexOf(':')+1) : null);
+        let resObj = {};
+        for (let i = 0; i < portList.length; i++) {
+            if (portList[i]) {
+                resObj[portList[i]] = {
+                    'port': portList[i],
+                    'process_name': serverDataList[i][1].slice(serverDataList[i][1].indexOf('/')+1),
+                    'pid': serverDataList[i][1].slice(0, serverDataList[i][1].indexOf('/')),
+                };
+            };
+        };
 
+        return {
+            serverData: resObj, 
+            resArr: resArr,
+        };
+    })
     
+
+
     .then(x => {
         res.status(200);
         res.send(x);
